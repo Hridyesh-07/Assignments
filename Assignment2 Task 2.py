a004cf71093cecd6f34f5d0f8987d6dc6feae880
Hridyesh-07 <hridyeshb8@gmail.com>
2025-05-12 17:55:03 +0530

    Add files via upload

---
+++ Assignment2 Task 2.py
@@ -0,0 +1,3 @@
+for number in range(0,51):
+    result = sum(range(1,51))
+print("The sum of integers from 1 to 50 is:", result)
---
+++ Assignment2 Task1.py
@@ -0,0 +1,6 @@
+Value=int(input('Enter a Value:'))
+if Value % 2 == 0:
+    print(str(Value)+' is an even number')
+else:
+    print(str(Value)+' is an odd number')
+print('Thank You')
