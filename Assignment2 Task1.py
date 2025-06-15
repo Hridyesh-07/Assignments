cba6b8361e32b63372bff9bd148344b2bd8e6291
Hridyesh-07 <hridyeshb8@gmail.com>
2025-06-16 00:17:55 +0530

    Update Assignment2 Task1.py

--- Assignment2 Task1.py
+++ Assignment2 Task1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 Value=int(input('Enter a Value:'))
 if Value % 2 == 0:
     print(str(Value)+' is an even number')
@@ -10,5 +9,4 @@ if Value % 2 == 0:
     print(str(Value)+' is an even number')
 else:
     print(str(Value)+' is an odd number')
->>>>>>> 5064f5605eed626cae472e96d23d5c707c24d045
 print('Thank You')
