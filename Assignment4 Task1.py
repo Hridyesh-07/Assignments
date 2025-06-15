13d2d63a01241016725b7292047382f950602c75
Hridyesh-07 <hridyeshb8@gmail.com>
2025-06-16 00:18:28 +0530

    Update Assignment4 Task1.py

--- Assignment4 Task1.py
+++ Assignment4 Task1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 try:
     file1 = open('sample.txt','r')
     reading_file1= file1.read()
@@ -6,7 +5,6 @@ try:
     file1.close()
 except FileNotFoundError:
     print("Error: The file 'sample.txt' not found.")
-=======
 try:
     file1 = open('sample.txt','r')
     reading_file1= file1.read()
@@ -14,4 +12,3 @@ try:
     file1.close()
 except FileNotFoundError:
     print("Error: The file 'sample.txt' not found.")
->>>>>>> 5064f5605eed626cae472e96d23d5c707c24d045
