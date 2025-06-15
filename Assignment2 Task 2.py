044666a43aa77bf6bba8c06f0ac908953486d9ad
Hridyesh-07 <hridyeshb8@gmail.com>
2025-06-16 00:17:34 +0530

    Update Assignment2 Task 2.py

--- Assignment2 Task 2.py
+++ Assignment2 Task 2.py
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 for number in range(0,51):
     result = sum(range(1,51))
 =======
 for number in range(0,51):
     result = sum(range(1,51))
->>>>>>> 5064f5605eed626cae472e96d23d5c707c24d045
 print("The sum of integers from 1 to 50 is:", result)
